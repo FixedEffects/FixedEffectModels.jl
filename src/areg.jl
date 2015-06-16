93c2ca07eafeea96ca30cb00b3e8d92276e9734d
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-16 17:28:02 -0400

    better comment

--- src/areg.jl
+++ src/areg.jl
@@ -3,10 +3,15 @@ using DataFrames, GLM
 
 
 function areg(f::Formula, df::AbstractDataFrame, absorb::Formula)
+	# do a deep copy of all variables present in f or absorb (except nothing)
 	allvars = [DataFrames.allvars(f), DataFrames.allvars(absorb)]
 	cols = setdiff(allvars, [:nothing])
-	ans = deepcopy(df[cols])
-	ans = demean!(ans, DataFrames.allvars(f), absorb)
+	df = deepcopy(df[cols])
+
+	# demean variables in f in place
+	df = demean!(df, DataFrames.allvars(f), absorb)
+
+	# Remove intercept from f and estimate linear model
 	terms = DataFrames.Terms(f)
 	terms.intercept = false
 	mf = ModelFrame(terms, ans)
