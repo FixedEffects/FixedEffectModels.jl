cdca6ff4149e467e9024f43d3b850ae867953485
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-24 18:37:51 -0400

    remove demean/regife

--- src/nonexported.jl
+++ src/nonexported.jl
@@ -1,3 +1,79 @@
+##############################################################################
+##
+## Demean functions (will export when DataFrames are in better shape)
+##
+##############################################################################
+
+
+function helper_demean!(out::AbstractDataFrame, df::AbstractDataFrame, cols::Vector{Symbol}, absorb::Formula)
+	
+	# construct subdataframe wo NA
+	condition = complete_cases(df)
+	subdf = sub(df, condition)
+	
+	# construct an array of factors
+	push!(factors, construct_fe(subdf, DataFrames.Terms(absorb).terms, sqrtw))
+
+	# case where only interacted fixed effect : add constant
+	if all(map(z -> typeof(z) <: FeInteracted, factors))
+		push!(factors, Fe(PooledDataArray(fill(1, size(subdf, 1))), sqrtw, :cons))
+	end
+	
+	# demean each vector sequentially
+	for x in cols
+		v = demean_vector(subdf[x], factors)
+		out[x] = DataVector(eltype(v), size(out, 1))
+		out[condition, x] = v
+	end
+	out
+end
+
+function helper_demean!(out::AbstractDataFrame, df::AbstractDataFrame, cols::Vector{Symbol})
+	# construct subdataframe wo NA
+	condition = complete_cases(df)
+	subdf = sub(df, condition)
+	for x in cols
+		out[condition, x] = subdf[x] .- mean(subdf[x])
+		out[!condition, x] = NA
+	end
+	return(out)
+end
+
+#
+# Exported function
+#
+
+function demean!(df::AbstractDataFrame, cols::Vector{Symbol}, absorb::Formula)
+	helper_demean!(df, df, cols, absorb)
+end
+
+function demean(df::AbstractDataFrame, cols::Vector{Symbol}, absorb::Formula)
+	out = DataFrame(Float64, size(df, 1), length(cols))
+	names!(out, cols)
+	helper_demean!(out, df, cols, absorb)
+	out
+end
+
+function demean!(df::AbstractDataFrame, cols::Vector{Symbol})
+	helper_demean(df, df, cols)
+end
+
+function demean(df::AbstractDataFrame, cols::Vector{Symbol})
+	out = DataFrame(Float64, size(df, 1), length(cols))
+	names!(out, cols)
+	helper_demean!(out, df, cols)
+end
+
+
+##############################################################################
+##
+## Factor models (Bai 2009)
+##
+##############################################################################
+
+
+
+
 
 type FactorModel <: FeModel
     id::Symbol
@@ -147,3 +223,5 @@ function estimate_factor_model(b::Vector{Float64}, X::Matrix{Float64}, M::Matrix
     scale!(F, sqrt(length(time.pool)))
     FactorEstimate(b, FactorStructure(id, time, Lambda, F))
 end
+
+
