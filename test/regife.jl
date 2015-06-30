c1b8d1934028873f06ce14e0eae954fa380f8867
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-30 10:48:04 -0400

    just write down the test

--- test/regife.jl
+++ test/regife.jl
@@ -6,18 +6,3 @@ df[:pYear] = pool(df[:Year])
 
 
 result = fit(InteractiveFixedEffectModel(:pState, :pYear, 2), Sales ~ Price, df, VcovSimple())
-
-
-residual = result.lambda * result.ft
-res_vector = similar(df[:Sales])
-for i in 1:length(res_vector)
-    res_vector[i] = residual[result.id.refs[i], result.time.refs[i]]
-end
-df[:res] = res_vector
-
-df[:pState] = pool(df[:State])
-
-df[:Salesm] = partial_out(Sales ~ res, df)[1]
-df[:Pricem] = partial_out(Price ~ res, df)[1]
-reg(Sales~Price + res, df, VcovCluster(:pState))
-reg(Salesm ~ Pricem, df, VcovCluster(:pState))
