c3e2221cdf72024a4c164c3008b50038fc2f8c55
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-30 17:49:44 -0400

    test

--- test/regife.jl
+++ test/regife.jl
@@ -4,7 +4,5 @@ df = dataset("plm", "Cigar")
 df[:pState] = pool(df[:State])
 df[:pYear] = pool(df[:Year])
 
-0.348820780059964]
-result = reg(Sales ~ Price, df, InteractiveFixedEffectModel(:pState, :pYear, 2))
 @test_approx_eq reg(Sales ~ Price, df, InteractiveFixedEffectModel(:pState, :pYear, 2)).coef [-0.348820780059964]
 @test_approx_eq reg(Sales ~ Price |> pState, df, InteractiveFixedEffectModel(:pState, :pYear, 2)).coef  [-0.42538935900021146]
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,15 @@
-fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
-quiet = length(ARGS) > 0 && ARGS[1] == "-q"
-anyerrors = false
-tests = ["reg.jl", "RegressionResult.jl", "partial_out.jl", "utils.jl", "regife.jl"]
+using FixedEffectModels
 
+tests = ["reg", 
+		 "RegressionResult", 
+		 "partial_out", 
+		 "utils", 
+		 "regife"]
 
 println("Running tests:")
+
 for t in tests
-    try
-        include(t)
-        println("\t\033[1m\033[32mPASSED\033[0m: $t")
-    catch
-        anyerrors = true
-        println("\t\033[1m\033[31mFAILED\033[0m: $t")
-    end
+    tfile = string(t, ".jl")
+    println(" * $(tfile) ...")
+    include(tfile)
 end
