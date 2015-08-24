ccddea52a6bafd19c9ecdbb187d720ef2aecc4bb
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-08-24 14:33:56 -0400

    update for 0.3

--- src/utils/weight.jl
+++ src/utils/weight.jl
@@ -9,9 +9,9 @@ type Ones <: AbstractVector{Float64}
     length::Int
 end
 Base.size(O::Ones) = O.length
-Base.getindex(O::Ones, ::Int...) = one(Float64)
+Base.getindex(::Ones, i::Int...) = one(Float64)
 # Add in version 0.4 unsafe_getindex
-Base.broadcast!{T}(o::Function, ::Array{Float64, T}, ::Array{Float64, T}, ::Ones) = nothing
+Base.broadcast!{T}(::Function, ::Array{Float64, T}, ::Array{Float64, T}, ::Ones) = nothing
 Base.scale!(::Vector{Float64}, ::Ones) = nothing
 
 
