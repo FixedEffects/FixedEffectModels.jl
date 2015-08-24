328a42c6f53a465f5174dee114c4a47339c2297e
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-08-24 14:21:02 -0400

    try

--- src/utils/weight.jl
+++ src/utils/weight.jl
@@ -5,21 +5,17 @@
 ## 
 ##############################################################################
 
-# http://stackoverflow.com/a/30968709/3662288
-immutable Ones <: AbstractVector{Float64}
-	length::Int
+type Ones <: AbstractVector{Float64}
+    length::Int
 end
 Base.size(O::Ones) = O.length
-Base.getindex(O::Ones, ::Int...) = one(Float64)
+Base.getindex(::Ones, ::Int...) = one(Float64)
 # Add in version 0.4 Base.unsafe_getindex(::Ones, ::Int...) = 1
-Base.broadcast!{T}(a::Function, b::Array{Float64, T}, c::Array{Float64, T}, d::Ones) = nothing
-Base.scale!(a::Vector{Float64}, b::Ones) = nothing
+Base.broadcast!{T}(o::Function, ::Array{Float64, T}, ::Array{Float64, T}, ::Ones) = nothing
+Base.scale!(::Vector{Float64}, ::Ones) = nothing
 
 
-function get_weight(df::AbstractDataFrame, weight::Symbol)
-	w = convert(Vector{Float64}, df[weight])
-	sqrtw = sqrt(w)
-end
+get_weight(df::AbstractDataFrame, weight::Symbol) = sqrt(df[weight])
 get_weight(df::AbstractDataFrame, ::Nothing) = Ones(size(df, 1))
 
 
