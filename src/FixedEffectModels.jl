67c0ca26fd566ffbf65d73c6bc2fc780b68762f0
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-09-01 14:08:30 -0400

    avoid pool

--- src/FixedEffectModels.jl
+++ src/FixedEffectModels.jl
@@ -66,7 +66,9 @@ include("partial_out.jl")
 
 let
 	# precompile various methods of demean
-	df = DataFrame(x = rand(10), y = rand(10), w = abs(rand(10)), id = pool(repeat(collect(1:2), inner = [5])));
+	df = DataFrame(x = rand(10), y = rand(10), w = abs(rand(10)), 
+		id = PooledDataArray(
+			RefArray(vcat(fill(one(Int), 5), fill(2 * one(Int), 5))), [1, 2]))
 	reg(y~x |> id, df)
 	reg(y~1|> id&x, df)
 	nothing
