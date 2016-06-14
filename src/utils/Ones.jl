f2bd67574f3cd736b4ec6d1a6106c95d2b23c9aa
matthieugomez <gomez.matthieu@gmail.com>
2016-06-14 11:19:47 -0400

    add shape

--- src/utils/Ones.jl
+++ src/utils/Ones.jl
@@ -30,7 +30,9 @@ Base.diagm{T}(o::Ones{T}, args...) = eye(T, O.length, args...)
 Base.sum(O::Ones) = O.length
 Base.convert{T}(::Type{Vector{T}}, o::Ones) = ones(T, length(o))
 Base.collect{T}(o::Ones{T}) = ones(T, length(o))
-
+if VERSION > v"0.5.0-"
+	Base.shape{T}(o::Ones{T}) = Tuple{Int}(length(o))
+end
 
 # implement broadcast
 ## solve ambiguity
