5ec9d2ff3002a7cabf3b8c4c4e039fbd239ba07d
matthieugomez <gomez.matthieu@gmail.com>
2017-05-19 15:50:56 -0400

    weight

--- src/weight/weight.jl
+++ src/weight/weight.jl
@@ -3,12 +3,12 @@
 ## Weight
 ## 
 ##############################################################################
-function get_weight(df::AbstractDataFrame, esample::BitVector, weight::Symbol) 
+function get_weight(df::AbstractDataFrame, esample::Union{BitVector, Vector}, weight::Symbol) 
     out = df[esample, weight]
     # there are no NA in it. DataVector to Vector
     out = convert(Vector{Float64}, out)
     map!(sqrt, out, out)
     return out
 end
-get_weight(df::AbstractDataFrame, esample::BitVector, ::Void) = Ones{Float64}(sum(esample))
+get_weight(df::AbstractDataFrame, esample::Union{BitVector, Vector}, ::Void) = Ones{Float64}(sum(esample))
 
