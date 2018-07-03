96538f1975cf4a769d02d2052fdd89f63e7af134
matthieugomez <gomez.matthieu@gmail.com>
2018-07-03 11:02:50 -0400

    simplify vcovrobust

--- src/vcov/vcovrobust.jl
+++ src/vcov/vcovrobust.jl
@@ -11,33 +11,23 @@ function vcov!(v::VcovRobustMethod, x::VcovData)
     return sandwich(x.crossmatrix, S) 
 end
 
-function shat!(::VcovRobustMethod, x::VcovData{T, 1}) where {T}
-    X = x.regressors
-    res = x.residuals
-    Xu = scale!(res, X)
-    S = At_mul_B(Xu, Xu)
-    scale!(S, size(X, 1) / x.df_residual)
-    return S
-end
 
 # S_{(l-1) * K + k, (l'-1)*K + k'} = \sum_i X[i, k] res[i, l] X[i, k'] res[i, l']
-function shat!(::VcovRobustMethod, x::VcovData{T, 2}) where {T}
-    X = x.regressors
-    res = x.residuals
-    nobs = size(X, 1)
-    dim = size(X, 2) * size(res, 2)
-    S = fill(zero(Float64), (dim, dim))
-    temp = fill(zero(Float64), nobs, dim)
-    index = zero(Int)
-    for k in 1:size(X, 2), l in 1:size(res, 2)
-        index += 1
-        for i in 1:nobs
-            temp[i, index] = X[i, k]* res[i, l]
+function shat!(::VcovRobustMethod, x::VcovData{T, N}) where {T, N}
+    dim = size(x.regressors, 2) * size(x.residuals, 2)
+    X2 = fill(zero(Float64), size(x.regressors, 1), dim)
+    index = 0
+    for k in 1:size(x.residuals, 2)
+        for j in 1:size(x.regressors, 2)
+            index += 1
+            @inbounds @simd for i in 1:size(x.regressors, 1)
+                X2[i, index] = x.regressors[i, j] * x.residuals[i, k]
+            end
         end
     end
-    S = At_mul_B(temp, temp)
-    scale!(S, nobs / x.df_residual)
-    return S
+    S2 = At_mul_B(X2, X2)
+    scale!(S2, size(x.regressors, 1) / x.df_residual)
+    return S2
 end
 
 
