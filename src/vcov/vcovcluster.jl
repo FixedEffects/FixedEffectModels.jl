438ee812f0f88d626210e925ee17f1c91a3a8aa2
matthieugomez <gomez.matthieu@gmail.com>
2019-03-09 12:03:19 -0500

    Update vcovcluster.jl

--- src/vcov/vcovcluster.jl
+++ src/vcov/vcovcluster.jl
@@ -71,8 +71,8 @@ function pinvertible(A::Matrix, tol = eps(real(float(one(eltype(A))))))
     eigval, eigvect = eigen(Symmetric(A))
     small = eigval .<= tol
     if any(small)
-        warn("estimated covariance matrix of moment conditions not of full rank.
-                 model tests should be interpreted with caution.")
+        @warn "estimated covariance matrix of moment conditions not of full rank.
+                 model tests should be interpreted with caution."
         eigval[small] = 0
         return eigvect' * Diagonal(eigval) * eigvect
     else
