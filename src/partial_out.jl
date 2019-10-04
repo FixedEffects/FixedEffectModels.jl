b14b40950df26f2239fdb845e794140d0df79bb2
matthieugomez <gomez.matthieu@gmail.com>
2019-10-04 09:45:30 -0400

    Update partial_out.jl

--- src/partial_out.jl
+++ src/partial_out.jl
@@ -23,8 +23,7 @@ The regression model is estimated on only the rows where *none* of the dependent
 ```julia
 using  RDatasets, DataFrames, FixedEffectModels, Gadfly
 df = dataset("datasets", "iris")
-df.SpeciesC = categorical(df.Species)
-result = partial_out(df, @model(SepalWidth + SepalLength ~ 1 + fe(SpeciesC)), add_mean = true)
+result = partial_out(df, @model(SepalWidth + SepalLength ~ fe(Species)), add_mean = true)
 plot(layer(result[1], x="SepalWidth", y="SepalLength", Stat.binmean(n=10), Geom.point),
    layer(result[1], x="SepalWidth", y="SepalLength", Geom.smooth(method=:lm)))
 ```
