2c6dd29e47b64d166a3707156a5ca3668cc16735
Matthieu Gomez <gomez.matthieu@gmail.com>
2023-01-31 11:16:37 -0500

    Update FixedEffectModels.jl

--- src/FixedEffectModels.jl
+++ src/FixedEffectModels.jl
@@ -43,7 +43,6 @@ if ccall(:jl_generating_output, Cint, ()) == 1   # if we're precompiling the pac
         df = DataFrame(x1 = [1.0, 2.0, 3.0, 4.0], x2 = [1.0, 2.0, 4.0, 4.0], y = [3.0, 4.0, 4.0, 5.0], id = [1, 1, 2, 2])
         reg(df, @formula(y ~ x1 + x2))
         reg(df, @formula(y ~ x1 + fe(id)))
-        reg(df, @formula(y ~ x1), Vcov.cluster(:id))
     end 
 end
 
