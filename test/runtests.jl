77356440f9a4d2fb43b680e261fedc25f3c71676
Matthieu Gomez <gomez.matthieu@gmail.com>
2023-03-08 09:55:52 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,4 @@ using FixedEffectModels, Test
 @testset "fit" begin include("fit.jl") end
 @testset "predict" begin include("predict.jl") end
 @testset "partial out" begin include("partial_out.jl") end
-@testset "collinearity" begin include("partial_out.jl") end
+@testset "collinearity" begin include("collinearity.jl") end
