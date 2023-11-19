d7053ba3bf767c7696e18f7145a6599feef646f8
Matthieu Gomez <gomez.matthieu@gmail.com>
2023-11-19 11:08:20 -0500

    rmv combination

--- src/utils/basecol.jl
+++ src/utils/basecol.jl
@@ -1,32 +1,5 @@
 ##############################################################################
 ##
-## Combination behaves like [A B C ...] without forming it
-## 
-##############################################################################
-
-struct Combination{T} <: AbstractMatrix{T}
-    A::Tuple
-    cumlength::Vector{Int}
-end
-
-function Combination(A::Union{AbstractVector{T}, AbstractMatrix{T}}...) where {T}
-    Combination{T}(A, cumsum([size(x, 2) for x in A]))
-end
-
-Combination() = error("`Combination` requires at least one argument")
-
-Base.size(c::Combination) = (size(c.A[1], 1), c.cumlength[end])
-Base.size(c::Combination, i::Integer) = size(c)[i]
-
-function Base.view(c::Combination, ::Colon, j)
-    index = searchsortedfirst(c.cumlength, j)
-    newj = index == 1 ? j : j - c.cumlength[index-1]
-    view(c.A[index], :, newj)
-end
-
-
-##############################################################################
-##
 ## Returns base of [A B C ...]
 ## Important: it must be the case that it returns the in order, that is [A B A] returns [true true false] not [false true true]
 ## 
@@ -54,7 +27,11 @@ end
 
 # generalized 2inverse
 function invsym!(X::AbstractMatrix; has_intercept = false)
-    # The C value adjusts the check to the relative scale of the variable. The C value is equal to the corrected sum of squares for the variable, unless the corrected sum of squares is 0, in which case C is 1. If you specify the NOINT option but not the ABSORB statement, PROC GLM uses the uncorrected sum of squares instead. The default value of the SINGULAR= option, 107, might be too small, but this value is necessary in order to handle the high-degree polynomials used in the literature to compare regression routin
+    # Options from SAS
+    # The C value adjusts the check to the relative scale of the variable. 
+    # The C value is equal to the corrected sum of squares for the variable, unless the corrected sum of squares is 0, in which case C is 1. 
+    # If you specify the NOINT option but not the ABSORB statement, PROC GLM uses the uncorrected sum of squares instead. 
+    # The default value of the SINGULAR= option, 107, might be too small, but this value is necessary in order to handle the high-degree polynomials used in the literature to compare regression routines
     tols = max.(diag(X), 1)
     for j in 1:size(X, 1)
         d = X[j,j]
@@ -81,7 +58,6 @@ end
 function getcols(X::AbstractMatrix,  basecolX::AbstractVector)
     sum(basecolX) == size(X, 2) ? X : X[:, basecolX]
 end
-
 
 ##############################################################################
 # Auxiliary functions to find columns of exogeneous, endogenous and IV variables
